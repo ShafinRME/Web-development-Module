a9a40e9d489e6abbaab04faaa6dd053c5897584e
Md.Safin Ahmed <shafin.rmedu@gmail.com>
2023-12-23 10:40:53 +0600

    Module -17 js practice problem done

---
+++ Module-17/concatetion.js
@@ -0,0 +1,5 @@
+var word1 = 'I am going to be';
+var word2 = 'an awesome web developer';
+
+var word = word1 + ' ' + word2;
+console.log(word);
---
+++ Module-17/cost.js
@@ -0,0 +1,4 @@
+var givenTaka = 1000;
+var bazar = 300;
+var bakiTaka = givenTaka - bazar;
+console.log(bakiTaka);
