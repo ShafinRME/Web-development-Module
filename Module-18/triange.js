f1d8c3d1f91bc3831c8c83dde2e50f962483fe70
Md.Safin Ahmed <shafin.rmedu@gmail.com>
2023-12-24 08:57:19 +0600

    Module 18 problem is done

---
+++ Module-18/triange.js
@@ -0,0 +1,11 @@
+var num1 = 10;
+var num2 = 8;
+var num3 = 9;
+
+if (num1 == num2 || num1 == num3 || num2 == num3) {
+    console.log("Isosceles");
+}
+
+else {
+    console.log("Not Isosceles");
+}
